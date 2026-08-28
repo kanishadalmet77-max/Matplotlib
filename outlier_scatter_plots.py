6cd4c699264c85198199415e6665732524f79470
Kanisha Dalmet <kanishadalmet77@gmail.com>
2026-08-28 06:01:04 +0530

    Add new files to repository

---
+++ outlier_scatter_plots.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6]
+y = [10, 12, 15, 18, 20, 50]
+
+plt.scatter(x, y)
+
+plt.title("Data Distribution")
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.show()
--- subplots.py
+++ subplots.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 
-axes[0].plot([1,2,3],[10,20,30])
+fig, axes = plt.subplots(1, 2)
+
+axes[0].plot([1, 2, 3], [10, 20, 30])
 axes[0].set_title("Sales")
 
+axes[1].bar(["A", "B", "C"], [20, 35, 25])
+axes[1].set_title("Products")
 
+plt.tight_layout()
 plt.show()
